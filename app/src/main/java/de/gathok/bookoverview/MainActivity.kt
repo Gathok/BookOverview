d70bb56d8115d7d51dbf088d7314f816076a800d
Gathok <gathok1787@gmail.com>
2025-02-23 15:44:20 +0100

    Adjust Menu Icons (#181)

--- app/src/main/java/de/gathok/bookoverview/MainActivity.kt
+++ app/src/main/java/de/gathok/bookoverview/MainActivity.kt
@@ -31,8 +31,6 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
